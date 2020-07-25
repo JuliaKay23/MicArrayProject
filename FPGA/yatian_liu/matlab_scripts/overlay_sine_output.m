7938bafe2cca7ea75e6e41d8182738c92af0e3d7
Yatian Liu <dougliu@umich.edu>
2020-07-25 18:45:26 -0400

    Add scripts to generate overlayed sine waves

--- FPGA/yatian_liu/matlab_scripts/overlay_sine_output.m
+++ FPGA/yatian_liu/matlab_scripts/overlay_sine_output.m
@@ -3,12 +3,13 @@ duration = 2e-3; % 2 ms duration
 
 % Generate PDM signal.
 t_pdm = 0:(1/Fs_pdm):duration;
+t_pdm(end) = []; % Remove the last sample point to get a whole length
 len = length(t_pdm);
 analog_sig = overlay_sine(t_pdm);
 fft_plot(analog_sig, Fs_pdm);
+
 pdm_sig = zeros(1, len);
 quant_error = 0;
-
 for i = 1:len
     if analog_sig(i) >= quant_error
         pdm_sig(i) = 1;
@@ -17,7 +18,8 @@ for i = 1:len
     end
     quant_error = quant_error + pdm_sig(i) - analog_sig(i);
 end
-
+fft_plot(pdm_sig, Fs_pdm);
+pdm_file_output(pdm_sig, 'overlay_sine.mif');
 
 % Calculate the normalized overlayed sine signal given a time array t.
 function signal = overlay_sine(t)
